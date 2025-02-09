7130a6b7e2051754217928d51a984852845f94a0
chuy-zip <andreschuy03@gmail.com>
2025-02-08 19:39:25 -0600

    graph for exercise

--- task_2_3.py
+++ task_2_3.py
@@ -4,17 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import classification_report
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.datasets import load_iris
 
 iris = load_iris()
 
-X_selected = iris.data[:, [0, 1]]  # Co
+X_selected = iris.data[:, [0, 1]]  # seleccionar columnas especificas
 
 y = iris.target
 
 trainingX, testX, trainY, testY = train_test_split(X_selected, y, test_size=0.2, random_state=42)
 
-perceptron = Perceptron(max_iter=100, eta0=0.1, random_state=42)
+perceptron = Perceptron(max_iter=1000, eta0=0.1, random_state=42)
 perceptron.fit(trainingX, trainY)
 
 predictions = perceptron.predict(testX)
@@ -22,3 +25,24 @@ predictions = perceptron.predict(testX)
 results = classification_report(testY, predictions)
 
 print(results)
+
+x_min, x_max = trainingX[:, 0].min() - 1, trainingX[:, 0].max() + 1
+y_min, y_max = trainingX[:, 1].min() - 1, trainingX[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
+                np.arange(y_min, y_max, 0.01))
+
+Z = perceptron.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+# grafica
+plt.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.Paired)
+plt.scatter(trainingX[:, 0], trainingX[:, 1], c=trainY, edgecolors='k', marker='o', cmap=plt.cm.Paired)
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
+plt.title('Perceptron Decision Boundary')
+plt.show()
+
+
+# Hay algo muy importante a tener en cuenta y es que la columna objetivo tiene 3 posibles valores "setosa, versicolor, virginica"
+# Es por eso que la neurona/perceptron esta teniendo una gran dificultad en las predicciones
+# ya que el perceptron es un clasificador binario
