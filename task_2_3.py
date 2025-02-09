d69ea4f93ab06cbc7a45f515872c50854ef97867
chuy-zip <andreschuy03@gmail.com>
2025-02-08 19:05:01 -0600

    balancing data

--- task_2_3.py
+++ task_2_3.py
@@ -1,1 +1,15 @@
 # file for a simple implentation about perceptrons
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+X_selected = iris.data[:, [0, 1]]  # Co
+
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=0.2, random_state=42)
