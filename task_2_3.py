f8d9fe0fbdeb2abc432e7d3d431721bbd1819901
chuy-zip <andreschuy03@gmail.com>
2025-02-08 19:13:39 -0600

    firs prediction attempt, graph pending

--- task_2_3.py
+++ task_2_3.py
@@ -2,7 +2,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 from sklearn.datasets import load_iris
 
@@ -12,4 +12,13 @@ X_selected = iris.data[:, [0, 1]]  # Co
 
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=0.2, random_state=42)
+trainingX, testX, trainY, testY = train_test_split(X_selected, y, test_size=0.2, random_state=42)
+
+perceptron = Perceptron(max_iter=100, eta0=0.1, random_state=42)
+perceptron.fit(trainingX, trainY)
+
+predictions = perceptron.predict(testX)
+
+results = classification_report(testY, predictions)
+
+print(results)
